61c33873c6479a4fe9e0f2546fc2a8f17054a4ac
Nakajima Kazuki <nakajima.k0301@gmail.com>
2019-11-06 09:32:48 +0900

    update

--- cachelog/database.py
+++ cachelog/database.py
@@ -26,12 +26,11 @@ class Database():
 
         with connection.cursor() as cursor:
             sql = """
-            SELECT
-                (SELECT VARIABLE_VALUE FROM INFORMATION_SCHEMA.GLOBAL_STATUS WHERE VARIABLE_NAME = 'QCACHE_HITS')/(SELECT SUM(VARIABLE_VALUE)
-            FROM
-                INFORMATION_SCHEMA.GLOBAL_STATUS
-            WHERE
-                VARIABLE_NAME IN ('QCACHE_HITS','QCACHE_INSERTS','QCACHE_NOT_CACHED'))*100 AS CACHE_HIT_RATE;
+            SELECT (SELECT VARIABLE_VALUE FROM INFORMATION_SCHEMA.GLOBAL_STATUS
+            WHERE VARIABLE_NAME = 'QCACHE_HITS')/(SELECT SUM(VARIABLE_VALUE)
+            FROM INFORMATION_SCHEMA.GLOBAL_STATUS WHERE VARIABLE_NAME
+            IN ('QCACHE_HITS','QCACHE_INSERTS','QCACHE_NOT_CACHED'))*100
+            AS CACHE_HIT_RATE;
             """
             cursor.execute(sql)
             return cursor.fetchone()
